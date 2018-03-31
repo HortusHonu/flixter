7fc79cae189910a4288482c5c6e1ca76bcca6110
Eric <james.eric.knight@gmail.com>
2018-03-31 22:07:06 +0000

    Lesson Drag and Drop Completed

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,6 @@
 class LessonsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_section, only: [:new, :create]
-	before_action :require_authorized_for_current_lesson, only: [:update]
+	before_action :require_authorized_for_current_section, only: [:show]
 
 	def new
 		@lesson = Lesson.new
@@ -12,22 +11,7 @@ class LessonsController < ApplicationController
 		redirect_to instructor_course_path(current_section.course)
 	end
 
-	def update
-		current_lesson.update_attributes(lesson_params)
-		render plain: 'updated!'
-	end
-
 	private
-
-	def current_lesson
-		@current_lesson ||= Lesson.find(params[:id])
-	end
-
-	def require_authorized_for_current_lesson
-		if current_lesson.section.course.user != current_user
-			render plain: 'Unathorized', status: :unauthorized
-		end
-	end
 
 	def require_authorized_for_current_section
 		if !current_user.enrolled_in?(current_lesson.section.course)
@@ -41,5 +25,5 @@ class LessonsController < ApplicationController
 	end
 
 	def lesson_params
-		params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+		params.require(:lesson).permit(:title, :subtitle, :video)
 	end
